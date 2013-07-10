a82f3f09267a350c1b5c991a3f03edc6b9dc8a29
Nishant_Pathak <nishantpathak.cse@gmail.com>
2013-07-10 18:00:55 +0000

    changing input from file:wq

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
+var buf;
+fs.readFile('index.html', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    buf = new Buffer(data.length);
+    buf.write(data);
+  });
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
